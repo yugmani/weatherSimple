465f835c60ec11d7c7677218260e0aa690578458
Himalaya <yugmani@yahoo.com>
2020-07-06 11:50:19 -0700

    finalized

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # weatherSimple
+
+ ## Weather App
+ ### TODO: Complete getWeatherData() to return json response Promise
+ Retrieve weather data from openweathermap
+ * HINT: Use fetch()
+ * HINT: URL should look like this: 
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ 
+ ### TODO: Complete searchCity() to get user input and get data using getWeatherData()
+ * Retrieve city input and get the weather data
+ * HINT: Use the promise returned from getWeatherData()
+ ### TODO: Complete showWeatherData() to set the data in the the html file from response
+ * Show the weather data in HTML
+ * HINT: make sure to console log the weatherData to see how the data looks like
+ 
+ ### Kelvin to Fahrenheit Conversion Formula
+ * ```Fahrenheit = (Kelvin− 273.15) × 9/5 + 32```
+
+ ### API URL
+[Open Weather API](api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key});
+ 
+ 
--- script.js
+++ script.js
@@ -1,68 +1,100 @@
-/**
- * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
- */
 
 // API_KEY for maps api
 
-let MY_URL = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}';
-/**
- * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this: 
- * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
- */
+let MY_KEY = "";
 
+// Function to get weather data from url using api key.
 const getWeatherData = (city) => {
     
-  const URL = "https://api.openweathermap.org/data/2.5/weather";
-  //HINT: Use template literals to create a url with input and an API key
-
-  //CODE GOES HERE
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
-  .then(response=>{
-      console.log(response);
-      return response.json()
-  })
-  .then(data=>{
-      console.log(data);
-      return data;
-  })
+    if (MY_KEY === ""){
+      var answer = confirm("Do you have your weather API key?");
+      if(answer){
+      MY_KEY = prompt("Enter your API key [https://openweathermap.org/]");
+      }
+      else {
+        alert("First get your API key from https://openweathermap.org/");
+        return;
+      }
+    }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${MY_KEY}`)
+    .then(response=>{
+        return response.json()
+    })
+    .then(weatherData=>{
+        console.log(weatherData);
+        showWeatherData(weatherData);
+    })
 };
 
-/**
- * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
- */
-
-// const cityInput = document.getElementById('city-input');
-// console.log(cityInput);
+// Function to get user input as a name of city
 const searchCity = () => {
-    var searchEl = document.querySelector('#search-button');
-    // console.log(searchEl);
-    document.querySelector('#search-button').addEventListener('click', function(e) {
+    var searchEl = document.querySelector('#search-icon');
+    searchEl.addEventListener('click', function(e) {
         e.preventDefault();
         const cityInput = document.getElementById('city-input');
         const city = cityInput.value;
-        console.log(city);
-        // return city;
         getWeatherData(city);
     })
-  
-  // CODE GOES HERE
  
 };
 
-/**
- * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
- */
+// Function to display the weather information in HTML 
 const showWeatherData = (weatherData) => {
-  //CODE GOES HERE
-  
+    const cityEl = document.getElementById("city-name");
+    const weatherType = document.getElementById("weather-type");
+    const tempEl = document.getElementById("temp");
+    const minTemp = document.getElementById("min-temp");
+    const maxTemp = document.getElementById("max-temp");
+    const humidityEl = document.getElementById("humidity");
+    cityEl.textContent = weatherData.name;
+    let weatherMain = weatherData.weather[0].main;
+    weatherType.textContent = weatherMain;
+
+    humidityEl.textContent = weatherData.main.humidity;
+    tempEl.textContent = Math.round((weatherData.main.temp - 273.15) * 9/5 + 32);
+    minTemp.textContent = Math.round((weatherData.main.temp_min - 273.15) * 9/5 + 32);
+    maxTemp.textContent = Math.round((weatherData.main.temp_max - 273.15) * 9/5 + 32);
+    weatherImage(weatherMain, weatherArray);
 };
 
 
-searchCity()
+// Initialize the city search function. 
+searchCity();
+
+// Array of weather types
+var weatherArray = [
+  "clear",
+  "clouds",
+  "drizzle",
+  "rain",
+  "snow",
+  "storm",
+  "thunderstorm",
+  "sunny"
+]
+
+// Function to insert image based on weathertype.
+const weatherImage = (type, array)=>{
+  const imageEl = document.getElementById("weather-image");
+  let src = "";
+  let img = "";
+  if (imageEl.hasChildNodes()) {
+    imageEl.removeChild(imageEl.childNodes[0]);
+  }
+
+  for (i=0; i<= array.length; i++){
+      if (type.toLowerCase() === array[i]){
+         src = `./images/${array[i]}.png`;
+      } 
+  }
+  if(src==="") {
+    src = "./images/searchicon.png";
+  } 
+  console.log(src);
+  
+  img = document.createElement("img");
+  img.setAttribute("src", src);
+  img.style.width = "80px";
+  imageEl.appendChild(img);
+  
+  }
